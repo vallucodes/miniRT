3b6dec1b2895e42d7a6c55d72814d9eba62ca606
Vallu <vallu9@hotmail.com>
2025-05-11 19:09:04 +0300

    add cannon

--- inc/raytracing.h
+++ inc/raytracing.h
@@ -1,7 +1,7 @@
 #ifndef RAYTRACING_H
 # define RAYTRACING_H
 
-typedef struct s_point_vector
+typedef struct s_tuple
 {
 	float	x;
 	float	y;
@@ -9,9 +9,21 @@ typedef struct s_point_vector
 	float	w;
 }	t_tuple;
 
+typedef struct s_env
+{
+	t_tuple	g;
+	t_tuple w;
+}	t_env;
+
+typedef struct s_proj
+{
+	t_tuple	pos;
+	t_tuple v;
+}	t_proj;
+
 t_tuple	create_vector(float x, float y, float z);
 t_tuple	create_point(float x, float y, float z);
-t_tuple	addition_tuples(t_tuple *tuple1, t_tuple *tuple2);
+t_tuple	addition_tuples(t_tuple tuple1, t_tuple tuple2);
 t_tuple substraction_tuples(t_tuple tuple1, t_tuple tuple2);
 t_tuple negate_tuple(t_tuple tuple1);
 t_tuple scalar_multiply_tuple(t_tuple tuple1, float scalar);
@@ -19,6 +31,8 @@ t_tuple scalar_divide_tuple(t_tuple tuple1, float scalar);
 float	magnitude_tuple(t_tuple tuple);
 t_tuple	normalize_tuple(t_tuple tuple);
 float	dot_tuple(t_tuple tuple1, t_tuple tuple2);
+t_tuple	cross_tuple(t_tuple tuple1, t_tuple tuple2);
+void	tick(t_env env, t_proj *proj);
 
 int	is_equal(float a, float b);
 
