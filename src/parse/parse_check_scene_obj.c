b8615c745d15dc1e4b532ba007e2d55d1a9614f4
elehtone <elehtone@student.hive.fi>
2025-05-19 19:59:10 +0300

    optical and scene objects parsing from valid file seems OK. TODO: error handling, error notification, memory.

--- src/parse/parse_check_scene_obj.c
+++ src/parse/parse_check_scene_obj.c
@@ -1,44 +1,20 @@
 #include "../../inc/minirt.h"
 
 /**
- * @brief Checks the current line is a valid sp (sphere) line. 
- * @param [in] *line: the current line of the scene file. 
- * @param [in] ps: t_parse, parsing struct. 
- * @return True if the line is a proper sp line, false otherwise. 
- * @note Pattern is: sp	 [x],[y],[z]	[0.0,?]	[0,255],[0,255],[0,255]
- * - allocates memory to **words via line_split_set(). 
- */
-bool	parse_check_sphere(char *line, t_parse *ps)
-{
-	t_sphere	*tmp;
-	char		**words;
-
-	ft_memset(&tmp, 0, sizeof(t_sphere));
-	words = line_split_set(line, " \t");
-	if (!parse_check_coords(words[1], &tmp->cx, &tmp->cy, &tmp->cz))
-		return (false);
-	if (!parse_check_geometry(words[2], &tmp->dia))
-		return (false);
-	if (!parse_check_rgb(words[3], &tmp->r, &tmp->g, &tmp->b))
-		return (false);
-	parse_add_obj_list(tmp, ps);
-	return (true);
-}
-
-/**
  * @brief Checks the current line is a valid pl (plane) line. 
  * @param [in] *line: the current line of the scene file. 
  * @param [in] ps: t_parse, parsing struct. 
  * @return True if the line is a proper pl line, false otherwise. 
  * @note Pattern is: pl [x],[y],[z] [-1,1],[-1,1],[-1,1] [0,255],[0,255],[0,255]
- * - allocates memory to **words via line_split_set(). 
+ * - allocates memory to *tmp via ft_calloc()
+ * - allocates memory to **words via line_split_set()
  */
 bool	parse_check_plane(char *line, t_parse *ps)
 {
-	t_plane	*tmp;
+	t_scene_obj	*tmp;
 	char	**words;
 
-	ft_memset(&tmp, 0, sizeof(t_plane));
+	tmp = ft_calloc(1, sizeof(t_scene_obj));
 	words = line_split_set(line, " \t");
 	if (!parse_check_coords(words[1], &tmp->cx, &tmp->cy, &tmp->cz))
 		return (false);
@@ -46,6 +22,34 @@ bool	parse_check_plane(char *line, t_parse *ps)
 		return (false);
 	if (!parse_check_rgb(words[3], &tmp->r, &tmp->g, &tmp->b))
 		return (false);
+	tmp->type = 0;
+	parse_add_obj_list(tmp, ps);
+	return (true);
+}
+
+/**
+ * @brief Checks the current line is a valid sp (sphere) line. 
+ * @param [in] *line: the current line of the scene file. 
+ * @param [in] ps: t_parse, parsing struct. 
+ * @return True if the line is a proper sp line, false otherwise. 
+ * @note Pattern is: sp	 [x],[y],[z]	[0.0,?]	[0,255],[0,255],[0,255]
+ * - allocates memory to *tmp via ft_calloc()
+ * - allocates memory to **words via line_split_set()
+ */
+bool	parse_check_sphere(char *line, t_parse *ps)
+{
+	t_scene_obj	*tmp;
+	char		**words;
+
+	tmp = ft_calloc(1, sizeof(t_scene_obj));
+	words = line_split_set(line, " \t");
+	if (!parse_check_coords(words[1], &tmp->cx, &tmp->cy, &tmp->cz))
+		return (false);
+	if (!parse_check_geometry(words[2], &tmp->dia))
+		return (false);
+	if (!parse_check_rgb(words[3], &tmp->r, &tmp->g, &tmp->b))
+		return (false);
+	tmp->type = 1;
 	parse_add_obj_list(tmp, ps);
 	return (true);
 }
@@ -56,14 +60,15 @@ bool	parse_check_plane(char *line, t_parse *ps)
  * @param [in] ps: t_parse, parsing struct. 
  * @return True if the line is a proper cy line, false otherwise. 
  * @note Pat: cy [x],[y],[z] [-1,1],[-1,1],[-1,1] [0.0,?] [0.0,?] [r],[g],[b]
- * - allocates memory to **words via line_split_set(). 
+ * - allocates memory to *tmp via ft_calloc()
+ * - allocates memory to **words via line_split_set()
  */
 bool	parse_check_cylinder(char *line, t_parse *ps)
 {
-	t_cylinder	*tmp;
+	t_scene_obj	*tmp;
 	char		**words;
 
-	ft_memset(&tmp, 0, sizeof(t_cylinder));
+	tmp = ft_calloc(1, sizeof(t_scene_obj));
 	words = line_split_set(line, " \t");
 	if (!parse_check_coords(words[1], &tmp->cx, &tmp->cy, &tmp->cz))
 		return (false);
@@ -75,6 +80,7 @@ bool	parse_check_cylinder(char *line, t_parse *ps)
 		return (false);
 	if (!parse_check_rgb(words[5], &tmp->r, &tmp->g, &tmp->b))
 		return (false);
+	tmp->type = 2;
 	parse_add_obj_list(tmp, ps);
 	return (true);
 }
