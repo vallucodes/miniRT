f501759996eb9711f3463e64eb8111f075593399
Vladimir Lopatinski <vlopatin@c3r5p1.hive.fi>
2025-06-17 15:15:03 +0300

    fix bug: bright single pixels in shadow

--- src/raytracing/shading.c
+++ src/raytracing/shading.c
@@ -14,6 +14,8 @@ t_xs	*intersect_world(t_minirt *minirt, t_ray r)
 		t_scene_obj *obj = (t_scene_obj *)temp->content;
 		intersect(obj, r, xs);
 		temp = temp->next;
+		if (temp == NULL)
+			break ;
 		i++;
 	}
 	return (xs);
@@ -28,7 +30,7 @@ t_comps	*prepare_computations(t_i i, t_ray r)
 	comps->point = position_ray(r, i.t);
 	comps->eyev = normalize_tuple(negate_tuple(r.dir));
 	comps->normalv = normal_at(comps->obj, comps->point);
-	comps->over_point = addition_tuples(comps->point, scalar_multiply_tuple(comps->normalv, EPSILON));
+	// comps->over_point = addition_tuples(comps->point, scalar_multiply_tuple(comps->normalv, EPSILON));
 	if (dot_tuple(comps->normalv, comps->eyev) < 0)
 	{
 		comps->inside = true;
@@ -41,7 +43,7 @@ t_comps	*prepare_computations(t_i i, t_ray r)
 
 t_color	shade_hit(t_parse *world, t_comps *comps, bool in_shadow)
 {
-	t_color	color = lighting(comps->obj->mat, world->lig_s, comps->over_point, comps->eyev, comps->normalv, in_shadow);
+	t_color	color = lighting(comps->obj->mat, world->lig_s, comps->point, comps->eyev, comps->normalv, in_shadow);
 	return (color);
 }
 
@@ -58,9 +60,7 @@ bool	is_shadowed(t_minirt *minirt, t_tuple point, t_scene_obj *obj)
 	dir = normalize_tuple(v);
 	r = create_ray(dir, point);
 	xs = intersect_world(minirt, r);
-	// print_xs(xs);
 	hit_p = hit(xs, obj);
-
 	if (xs->count != 0 && hit_p.t < distance && hit_p.t > 0)
 		return (true);
 	else
@@ -74,10 +74,9 @@ t_color	color_at(t_minirt *minirt, t_ray r)
 	if (hit_p.object == NULL)
 		return (color(0, 0, 0));
 	t_comps *comps = prepare_computations(hit_p, r);
-	bool	in_shadow = is_shadowed(minirt, comps->over_point, hit_p.object);
+	bool	in_shadow = is_shadowed(minirt, comps->point, hit_p.object);
 	t_color color = shade_hit(minirt->world, comps, in_shadow);
 	free(xs);
 	return (color);
 }
-
 
